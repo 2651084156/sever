e150d8b252d7751bfee30d96f7b623283941a4d2
autumn <2>
2022-02-11 19:00:11 +0800

    2

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+from starlette.requests import Request
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# 创建一个templates（模板）对象，以后可以重用。
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
@@ -15,3 +20,7 @@ async def say_hello(name: str):
 @app.get("/hello1")
 async def say_hello1():
     return '<h1>Hello World</h1>'
+
+@app.get("/items/{id}")
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
